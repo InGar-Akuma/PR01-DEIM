a3da352f6eee72d6e31085f8a08aa64fb4a797b5
Ines Garcia <stigracer666@gmail.com>
2021-09-21 13:17:52 +0200

    Ejercicio 2 b
    Contadores while y for.

--- Ejercicios_programacion/Assets/Scripts/Ejercicio2.cs
+++ Ejercicios_programacion/Assets/Scripts/Ejercicio2.cs
@@ -5,26 +5,22 @@ using UnityEngine;
 public class Ejercicio2 : MonoBehaviour
 {
 
-    int valor = 0;
+    int valor = 10;
 
 
     // Start is called before the first frame update
     void Start()
     {
-         
+        while (valor >= 0) 
+        {
+            print("El valor while es: " + valor);
+            valor--;
+        }
+
+        for (valor = 10; valor >= 0 ; valor--)
+        {
+            print("el valor for es: " + valor);
+        }
     }
 
-    void Update()
-    {
-        if (valor <= 100)
-        {
-            print(valor);
-            valor++;
-        }
-        if (valor == 101)
-        {
-            print("Es mayor que 100");
-            valor = 102;
-        }
-    }
 }
