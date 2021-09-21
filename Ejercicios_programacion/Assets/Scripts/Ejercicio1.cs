c20e086bef1ac70ea45818fba6eac356e901670b
Ines Garcia <stigracer666@gmail.com>
2021-09-21 11:16:34 +0200

    Ejercicio 1 c
    Operaciones con variables.

--- Ejercicios_programacion/Assets/Scripts/Ejercicio1.cs
+++ Ejercicios_programacion/Assets/Scripts/Ejercicio1.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class Ejercicio1 : MonoBehaviour
 {
-    public int num1 = 42;
     
-    [SerializeField] float decimal1 = 22.2f ;
+    [SerializeField] float decimal1;
+    [SerializeField] float decimal2;
+    [SerializeField] float decimal3;
 
-    public bool boolean1 = true;
-
-    [SerializeField] string texto1 = "ash nazg thrakatuluk agh burzum-ishi krimpatul";
+    private string texto1 = "Tus resultados son: ";
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //num1 = 23;
-        decimal1 = 33.3f;
-        //boolean1 = false;
-        //texto1 = "Hola mundo";
+        float resultadoSuma = decimal1 + decimal2 + decimal3;
+        float resultadoResta = decimal1 - decimal2 - decimal3;
+        float resultadoMult = decimal1 * decimal2 * decimal3;
+        float resultadoDiv = decimal1 / decimal2 / decimal3;
 
-        print(num1);
         print(texto1);
-        print(boolean1 + " " + decimal1);
+        print("suma: " + resultadoSuma + " ; resta: " + resultadoResta + " ; multiplicacion: " + resultadoMult + " ; division: " + resultadoDiv);
     }
 }
