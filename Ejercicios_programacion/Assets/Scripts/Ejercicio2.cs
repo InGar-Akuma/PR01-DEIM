6169ae871790a3d079b6bf9265834d07ac603675
Ines <stigracer666@gmail.com>
2021-09-28 09:59:14 +0200

    Ejercicio 2 d
    Numero al azar

--- Ejercicios_programacion/Assets/Scripts/Ejercicio2.cs
+++ Ejercicios_programacion/Assets/Scripts/Ejercicio2.cs
@@ -5,35 +5,23 @@ using UnityEngine;
 public class Ejercicio2 : MonoBehaviour
 {
 
-    [SerializeField] int valor;
+    [SerializeField] int numero;
+    float numAzar;  
 
 
     // Start is called before the first frame update
     void Start()
     {
-        while (valor >= 0) 
+       while(numAzar != numero)
         {
-            print(valor);
-
-            if (valor == 100)
-            {
-                print("Queda mucho");
-            }
-
-            if (valor == 5)
-            {
-                print("Queda poco");
-            }
-            
-
-            if (valor == 0) 
-            {
-                print("Has llegadoa cero");
-            }
-
-            valor--;
+            numAzar = Random.Range(0, 11);
+            print(numAzar);
         }
-         
+       
+       if (numAzar == numero)
+        {
+            print("¡Coinciden! " + numAzar);
+        }
 
     }
 
